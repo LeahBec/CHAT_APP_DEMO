307ebe79b75c068b9906343093305f7cd3d7650a
LeahBec <leah.beckerman@gmail.com>
2023-08-22 23:01:57 +0300

    Initial commit

---
+++ chatApp.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def homePage():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port='5000', debug='True')
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def homePage():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('homePage'))
+    return render_template('register.html', error=error)
