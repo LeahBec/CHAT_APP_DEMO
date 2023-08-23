ece88f9decc6fbf4d6f1086e915ec08f6c25e52e
LeahBec <leah.beckerman@gmail.com>
2023-08-24 01:12:58 +0300

    login works and nevigates

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ COPY requirements.txt .
 # install dependencies
 RUN pip install --trusted-host pypi.org --trusted-host files.pythonhosted.org -r requirements.txt
 # copy the content of the local src directory to the working directory
-COPY / .
+COPY . .
 # command to run on container start
 CMD [ "python", "./chatApp.py" ]
--- chatApp.py
+++ chatApp.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 import csv
 app = Flask(__name__)
+app.secret_key="secret_key"
 
 # @app.route('/')
 # def homePage():
@@ -17,7 +18,7 @@ def is_registered(name):
 def register_user(name, password):
       with open('users.csv', 'a') as myFile:
             writer = csv.writer(myFile)
-            writer.writerow([name, password.encode])
+            writer.writerow([name, password])
 @app.route('/register', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def homePage():   
@@ -34,18 +35,25 @@ def homePage():
     return render_template('register.html', error=error)
 
 
+def is_right_pass(name, password):
+         with open('users.csv', 'r') as myFile:
+            myReader = csv.reader(myFile)
+            for line in myReader:
+                if line[0] == name and line[1] == password:
+                     return True
+         return False
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
      
         name = request.form['username']
         password = request.form['password']
-        if is_registered(name):
-            return redirect('login')
+        if is_right_pass(name, password):
+            session['username'] = name
+            return redirect('lobby')
         else:
-            register_user(name, password)
-            return redirect('login')
-    
+            return "wrong pass!"
     return render_template('login.html')
 
 
