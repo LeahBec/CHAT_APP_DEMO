eca70ccb2d972265a21df0d8ad1f0e6ac8efc033
LeahBec <leah.beckerman@gmail.com>
2023-08-23 12:41:52 +0300

    Wednesday 12:41

--- chatApp.py
+++ chatApp.py
@@ -1,10 +1,43 @@
 from flask import Flask, render_template, redirect, url_for, request
-
+import csv
 app = Flask(__name__)
+USERS={}
 
 # @app.route('/')
 # def homePage():
 #     return render_template('register.html')
+@app.route('/', methods=['GET', 'POST'])
+def homePage():
+    error = None
+    is_registered = False
+    name = request.form['username']
+    password = request.form['password']
+    with open('users.csv', 'r') as myFile:
+        myReader = csv.reader(myFile)
+        for user in myReader:
+            if user == name:
+                is_registered = True
+        if name =
+    # if request.method == "POST":
+    #     name = request.form['username']
+    #     password = request.form['password']
+    #     return f"your name is {name} and your pass is {password}"
+            #return render_template("login.html")
+    # if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+    #     error = 'Invalid Credentials. Please try again.'
+    # else:
+    #     return redirect(url_for('homePage'))
+    return render_template('register.html', error=error)
+
+def main():
+     myList = []
+with open('users.csv', 'r') as myFile:
+    myReader = csv.reader(myFile)
+    myList = list(myReader)
+    
+print(myList)
+
+
 
 if __name__ == '__main__':
     app.debug = True
@@ -12,13 +45,3 @@ if __name__ == '__main__':
 
 
 
-@app.route('/', methods=['POST'])
-def homePage():
-    error = None
-    name = request.form['username']
-    password = request.form['password']
-    # if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-    #     error = 'Invalid Credentials. Please try again.'
-    # else:
-    #     return redirect(url_for('homePage'))
-    return render_template('register.html', error=error)
