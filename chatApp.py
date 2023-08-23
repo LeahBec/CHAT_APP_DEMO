f3ff4f69b916c3c65da3a1f429c856826a4f0110
LeahBec <leah.beckerman@gmail.com>
2023-08-23 13:36:19 +0300

    working on registry

--- chatApp.py
+++ chatApp.py
@@ -1,23 +1,36 @@
 from flask import Flask, render_template, redirect, url_for, request
 import csv
 app = Flask(__name__)
-USERS={}
 
 # @app.route('/')
 # def homePage():
 #     return render_template('register.html')
+def is_registered(name):
+    with open('users.csv', 'r') as myFile:
+            myReader = csv.reader(myFile)
+            for line in myReader:
+                if line[0] == name:
+                     return True
+                
+
+
+def register_user(name, password):
+      with open('users.csv', 'a') as myFile:
+            writer = csv.reader(myFile)
+            for line in myReader:
+                if line[0] == name:
+                     return True
+
 @app.route('/', methods=['GET', 'POST'])
 def homePage():
     error = None
     is_registered = False
     name = request.form['username']
     password = request.form['password']
-    with open('users.csv', 'r') as myFile:
-        myReader = csv.reader(myFile)
-        for user in myReader:
-            if user == name:
-                is_registered = True
-        if name =
+    if is_registered(name):
+         return redirect('login.html')
+    else:
+         register_user(name, password)
     # if request.method == "POST":
     #     name = request.form['username']
     #     password = request.form['password']
