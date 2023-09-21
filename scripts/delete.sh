f4f628084ebb7373bdc8780163802a05862f2848
LeahBec <leah.beckerman@gmail.com>
2023-09-22 00:35:43 +0300

    best practices plus 15 - bonus

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 docker stop chat-app-run
+docker stop chat-App-run
 if [ $# -eq 0 ]; then
     docker rmi -f chat-app
 else
     docker rmi -f chat-app:$1
 fi
 docker rm -f chat-app-run
+docker rm -f chat-App-run
 
 
